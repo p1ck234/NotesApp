9c783d29b2777aa0360d5d0451a25de1afd7e026
Danil Vetrov <danil-vetrov47@mail.ru>
2022-04-26 10:06:06 +0400

    Исправил баг при редактировании

--- NotesApp/WindowRed.xaml.cs
+++ NotesApp/WindowRed.xaml.cs
@@ -30,26 +30,28 @@ namespace NotesApp
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             Note currentNote = new Note();
-            if (tbTitle.LineCount < 50)
+            if (tbTitle.LineCount < 50 && tbBody.Text != "")
+            {
                 currentNote.Title = tbTitle.Text;
-            else
-                MessageBox.Show("Длина заголовка превышает 50 символов", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            if (tbBody.Text != "")
                 currentNote.Body = tbBody.Text;
-            else
-                MessageBox.Show("Нельзя сохранить пустую заметку", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            try
-            {
-                MainWindow.bd.Notes.Load();
-                MainWindow.bd.Notes.Remove(MainWindow.selectEntites);
-                MainWindow.bd.Notes.Add(currentNote);
-                MainWindow.bd.SaveChanges();
-                MessageBox.Show("Заметка сохранена", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
-                this.Close();
+                try
+                {
+                    MainWindow.bd.Notes.Load();
+                    MainWindow.bd.Notes.Remove(MainWindow.selectEntites);
+                    MainWindow.bd.Notes.Add(currentNote);
+                    MainWindow.bd.SaveChanges();
+                    MessageBox.Show("Заметка сохранена", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
-            catch (Exception ex)
+                
+            else
             {
-                MessageBox.Show(ex.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Длина заголовка превышает 50 символов или вы пытаетесь сохранить пустую заметку", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
